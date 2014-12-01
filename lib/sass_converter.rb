4c1472f25730ff07e2f1fb38dd8576bcd10232ed
Serj L <gelvior@gmail.com>
2014-12-01 20:28:10 +0300

    [sass converter] Module SassConverter added to project.

---
+++ lib/sass_converter.rb
@@ -0,0 +1,58 @@
+module SassConverter
+  module_function
+
+  # TODO: read file only if it changed (caching results).
+
+  def convert(file_path="#{Rails.root}/app/assets/stylesheets/variables.css.scss")
+    variables_list = read_variables(file_path)
+    variables_to_h(variables_list)
+  end
+
+  private
+
+  def self.read_variables(file_path)
+    variables_list = []
+
+    File.readlines(file_path).each do |line|
+      variables_list << line
+    end
+
+    variables_list
+  end
+
+  def self.variables_to_h(variables_list)
+    sass_hash = {}
+    variables_list.each do |line|
+      regexp_result = line.scan(/\$(.+):(\s*)(.+);$/).flatten                                       #$ any_symbols : any_space_symbols actual_value ;
+
+      if regexp_result.any?
+        var_name  = regexp_result[0]                                                                # First regexp group.
+        var_value = destringify(regexp_result[2])                                                   # Third regexp group.
+
+        sass_hash[var_name.to_sym] = var_value
+      end
+    end
+
+    Rails.logger.info "SassConverter: values converted: #{sass_hash}"
+
+    sass_hash
+  end
+
+  def self.destringify(value)
+    if int?(value)
+      value.to_i
+    elsif float?(value)
+      value.to_f
+    else
+      value
+    end
+  end
+
+  def self.int?(str)
+    str.to_i.to_s == str
+  end
+
+  def self.float?(str)
+    !!Float(str) rescue false
+  end
+end
---
+++ spec/lib/sass_converter_spec.rb
@@ -0,0 +1,17 @@
+describe "SassConverter" do
+  it "should return hash when conversion is succeeded" do
+    correct_data = SassConverter.convert("#{Rails.root}/spec/fixtures/valid_variables.css.scss")
+    expect(correct_data).to eq({ screen_xs_max: "767px",
+                                 screen_md_max: "1199px",
+                                 background_color: "rgba(255, 255, 255, 0.95) !important",
+                                 border_width: 0,
+                                 width: "90px",
+                                 line_height: 1.5,
+                                 screen_us_max: "$screen_md_max + 300" })
+  end
+
+  it "should raise error if variable is not correct" do
+    converted_data = SassConverter.convert("#{Rails.root}/spec/fixtures/invalid_variables.css.scss")
+    expect(converted_data).to eq({})
+  end
+end
